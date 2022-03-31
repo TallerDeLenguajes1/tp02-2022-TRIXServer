9c38a6901fbaff32113d96f403ed7f2ede3bddc0
TRIXServer <admin@trixserver.com>
2022-03-31 18:17:03 -0300

    commit

---
+++ tp2_1_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 20
+
+int main(int argc, char const *argv[])
+{
+    int vt[N];
+    for(int i = 0; i < N; i++)
+    {
+        vt[i] = 1 + rand() % 100;
+        printf("%d ", vt[i]);
+    }
+    return 0;
+}
