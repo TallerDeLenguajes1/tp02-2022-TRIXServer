16a256a03f848e144dff90f5aae98867bbc01874
TRIXServer <admin@trixserver.com>
2022-05-01 21:25:13 -0300

    commit

--- tp2_3.c
+++ tp2_3.c
@@ -4,17 +4,21 @@
 #define N 5
 #define M 7
 
-int i, j;
-int mt[N][M];
-
 int main(int argc, char const *argv[])
 {
+    int i, j;
+    int matriz[N][M];
+    int * pMatriz;
+
+    pMatriz = matriz;
+
+    system("cls || clear");
     for (i = 0; i < N; i++)
     {
         for (j = 0; j < M; j++)
         {
-            mt[i][j] = 1 + rand() % 100;
-            printf("%d\t", mt[i][j]);
+            * (pMatriz + (i * M + j)) = 1 + rand() % 100;
+            printf("%d\t", * (pMatriz + (i * M + j)));
 
         }
 
