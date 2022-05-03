bdd6810062938f651528a9ffb112196cebae5843
TRIXServer <admin@trixserver.com>
2022-05-03 20:08:23 -0300

    commit

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define N 20
+#define N 200
 
 int main(int argc, char const *argv[])
 {
     int vector[N];
     system("cls || clear");
+    srand(time(NULL));
+
     for(int i = 0; i < N; i++)
     {
         vector[i] = 1 + rand() % 100;
